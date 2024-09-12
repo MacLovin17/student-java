e9a7a809059d3f361ceedf002c982107e4429855
MacLovin17 <erikygomesuchiha@gmail.com>
2024-09-12 11:57:09 -0300

    POO

---
+++ carro.java
@@ -0,0 +1,11 @@
+public class carro{
+    String carro = "Volvo";
+}
+class Second{
+    public static void main(String[]args){
+        carro myObj = new carro();
+        
+        System.out.println(myObj.carro);
+        
+    }
+}
